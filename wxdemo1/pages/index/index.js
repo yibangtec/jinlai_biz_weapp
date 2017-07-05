fa5cdde23a7abaa017139d558e352938279aeb8f
roseQing0627 <rosese_qing0623@163.com>
2017-07-05 18:32:05 +0800

    wxdemo1.1

---
+++ wxdemo1/pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '自定义转发标题',
+      path: '/page/index'
+    }
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
