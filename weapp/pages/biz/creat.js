90619abd8e136b9b5ac0dd58a731e3f423acee59
roseQing0627 <rosese_qing0623@163.com>
2017-07-14 18:36:44 +0800

    2.0

--- weapp/pages/biz/creat.js
+++ weapp/pages/biz/creat.js
@@ -1,20 +1,79 @@
 // creat.js
+var user_id, tel, pw;
+const Upyun = require('../../utils/upyun-wxapp-sdk')
+const upyun = new Upyun({
+  bucket: 'jinlaisandbox-images',
+  operator: 'jinlaisandbox',
+  getSignatureUrl: 'http://123.103.113.100:9090/tradso/Warehouse/test'
+})
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    url:'',
+    display:'display:none',
+    array:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.getStorage({
+      key: 'user',
+      success: function (res) {
+        user_id = res.data.content.user_id
+      },
+      fail: function (err) {
+
+      }
+    })
   
   },
+  chooseImage: function () {
+    const self = this
 
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album'],
+      success: function (res) {
+        console.log('chooseImage success, temp path is', res.tempFilePaths[0])
+
+        const imageSrc = res.tempFilePaths[0]
+        upyun.upload({
+          localPath: imageSrc,
+          remotePath: '/biz/auth_doc',
+          success: function (res) {
+            console.log('uploadImage success, res is:', res)
+
+            wx.showToast({
+              title: '上传成功',
+              icon: 'success',
+              duration: 1000
+            })
+
+            self.setData({
+              imageSrc
+            })
+          },
+          fail: function ({ errMsg }) {
+            console.log('uploadImage fail, errMsg is', errMsg)
+          }
+        })
+      },
+      fail: function ({ errMsg }) {
+        console.log('chooseImage fail, err is', errMsg)
+      }
+    })
+  },
+  book:function(e){
+    var that=this
+    
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
